81ad6c3f714233401403b44ca82ca2ad9d603026
agg23 <adam@appcannonsoftware.com>
2022-08-10 15:17:10 -0700

    Fixed 7-segment displaying across the entire screen

--- src/fpga/core/pong/ic/7448_bcd_7_segment.vhd
+++ src/fpga/core/pong/ic/7448_bcd_7_segment.vhd
@@ -13,26 +13,28 @@ entity ic7448 is
 end entity;
 
 architecture rtl of ic7448 is
-
 begin
   process (input, not_blanking)
   begin
     if not_blanking = '1' then
       case input is
-        when x"0" => output <= b"1111110";
-        when x"1" => output <= b"0110000";
-        when x"2" => output <= b"1101101";
-        when x"3" => output <= b"1111001";
-        when x"4" => output <= b"0110011";
-        when x"5" => output <= b"1011011";
-        when x"6" => output <= b"0011111";
-        when x"7" => output <= b"1110000";
+        when x"0" => output <= b"0111111";
+        when x"1" => output <= b"0000110";
+        when x"2" => output <= b"1011011";
+        when x"3" => output <= b"1001111";
+        when x"4" => output <= b"1100110";
+        when x"5" => output <= b"1101101";
+        when x"6" => output <= b"1111100";
+        when x"7" => output <= b"0000111";
         when x"8" => output <= b"1111111";
-        when x"9" => output <= b"1110011";
+        when x"9" => output <= b"1100111";
+        when x"F" => output <= b"0000000";
           -- This isn't correct, but I want it to properly indicate that something went wrong
           -- so we'll display "C"
-        when others => output <= b"1001110";
+        when others => output <= b"0111001";
       end case;
+    else
+      output <= b"0000000";
     end if;
   end process;
 end architecture;
